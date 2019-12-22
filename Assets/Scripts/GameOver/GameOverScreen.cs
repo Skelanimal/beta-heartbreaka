e84e4580bcebc35ac9af19fc262d31f44633527f
Matt McCarthy <sk3lanimals@gmail.com>
2019-12-21 19:09:39 -0500

    kill barrier created to kill soyboy. gameOver screen created to represent gameOverText and reset game upon pressing spacebar.

---
+++ Assets/Scripts/GameOver/GameOverScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameOverScreen : MonoBehaviour
+{
+  public void gameOver()
+  {
+    GameObject.Find("GameOverText").GetComponent<Text>().enabled = true;
+    GameObject.Find("GameOverText").GetComponent<RestartFunction>().enabled = true;
+  }
+}
